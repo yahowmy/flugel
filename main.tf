982c1a3411d5e4f8b03b8192397ac9613a6a2a0b
yahowmy <72358484+yahowmy@users.noreply.github.com>
2021-09-30 00:25:34 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ resource "aws_instance" "flugel" {
     ami = var.ami_id
     instance_type = "t2.micro"
     key_name = "flugel"
-    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
     tags = {
         Name = var.instance_name
